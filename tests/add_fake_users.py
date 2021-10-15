c7603f3da92894caad8d4b5944b75792b72bdf55
anotherjump <anotherjump@gmail.com>
2021-10-15 14:54:31 -0400

    commit

---
+++ tests/add_fake_users.py
@@ -0,0 +1,30 @@
+import mariadb
+import faker
+NUMBER_OF_USERS_TO_ADD = 10
+
+
+def connect_db():
+    conn = mariadb.connect(
+        user="root",
+        password="dbpass",
+        host="127.0.0.1",
+        port=3307,
+        database="msgsrv"
+        )
+    return conn
+
+
+def fake_user_passwd(faker_factory):
+    return faker_factory.profile(fields=['username']).get('username'), faker_factory.password()
+
+
+if __name__ == "__main__":
+    ff = faker.Faker()
+    conn = connect_db()
+    assert conn, "no connection"
+    cur = conn.cursor()
+    for i in range(NUMBER_OF_USERS_TO_ADD):
+        data = fake_user_passwd(ff)
+        cur.execute("INSERT INTO users (name, password) VALUES (?, ?)", (data[0], data[1]))
+        conn.commit()
+        print(f"added fake user: {data}")
